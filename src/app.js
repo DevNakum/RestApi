23bf556f02889a03b3cd416eeb63c981bbdaf2c4
DevNakum <ndev2003@gmail.com>
2023-03-23 22:15:20 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 require("./db/conn");
-const Student = require("./models/students");
+// const Student = require("./models/students");
 const StudentRouter = require("./routers/students");
 const app = express();
 const port = process.env.PORT || 8000;
-const Admin = require("./routers/admin")
+// const Admin = require("./routers/admin")
 // epress can identify json data which is get to postman
 
 app.use(express.json());
